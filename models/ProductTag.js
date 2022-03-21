22cd5a8f9470ec4b8f0cd70cefa04079db3c0b5a
Namees Albayati <nameesalbayati@Nameess-MacBook-Pro.local>
2022-03-21 10:21:15 -0500

    adding models info

---
+++ models/ProductTag.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+  id:{
+    type:DataTypes.INTEGER,
+    AllowNull:false,
+    autoIncrement:true,
+    primaryKey:true
+
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references: { model: 'tag', key: 'id' }
+
+
+
+    }
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
